495d4f63ef20fd273ccc96f6884455eda2698d23
cunweizhao <18500103508@163.com>
2020-07-29 15:49:14 +0800

    框架搭建

---
+++ src/main/java/com/zcw/javafx/JavaFxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zcw.javafx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFxDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zcw/javafx/JavaFxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcw.javafx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
